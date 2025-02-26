e4b7825a9bf7803501f94757c3e684c6456c6028
Hamzeh Belaih <hbelaih@c2r6s5.42amman.com>
2025-02-26 16:02:31 +0300

    fully functional tokenizer tested by firas

--- src/parsing/tokens.c
+++ src/parsing/tokens.c
@@ -6,7 +6,7 @@
 /*   By: hbelaih <hbelaih@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 14:21:35 by hbelaih           #+#    #+#             */
-/*   Updated: 2025/02/25 16:20:28 by hbelaih          ###   ########.fr       */
+/*   Updated: 2025/02/26 14:39:21 by hbelaih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,14 +75,20 @@ t_token	*tokenize(const char *input)
 	int		i;
 	size_t	j;
 
+	tokens = NULL;
 	i = 0;
 	j = 0;
-	tokens = NULL;
 	while (input[i])
 	{
 		current_type = token_type(input, i);
 		if (current_type == TOKEN_WHITESPACE)
 		{
+			if (j > 0)
+			{
+				buffer[j] = '\0';
+				add_token(&tokens, buffer, TOKEN_WORD);
+				j = 0;
+			}
 			i++;
 			continue ;
 		}
@@ -92,9 +98,7 @@ t_token	*tokenize(const char *input)
 			buffer[j++] = quote_char;
 			i++;
 			while (input[i] && input[i] != quote_char)
-			{
 				buffer[j++] = input[i++];
-			}
 			if (input[i] == quote_char)
 			{
 				buffer[j++] = quote_char;
@@ -108,6 +112,11 @@ t_token	*tokenize(const char *input)
 		if ((current_type == TOKEN_HEREDOC || current_type == TOKEN_APPEND)
 			&& input[i + 1])
 		{
+			if (input[i + 2] == input[i])
+			{
+				ft_printf("Syntax error: too many redirection operators\n");
+				return (NULL);
+			}
 			buffer[0] = input[i];
 			buffer[1] = input[i + 1];
 			buffer[2] = '\0';
@@ -121,8 +130,13 @@ t_token	*tokenize(const char *input)
 			i += 2;
 			continue ;
 		}
-		if (current_type != TOKEN_WORD)
+		if (current_type == TOKEN_REDIR)
 		{
+			if (input[i + 1] == input[i])
+			{
+				ft_printf("Syntax error: too many redirection operators\n");
+				return (NULL);
+			}
 			if (j > 0)
 			{
 				buffer[j] = '\0';
@@ -132,12 +146,39 @@ t_token	*tokenize(const char *input)
 			buffer[0] = input[i];
 			buffer[1] = '\0';
 			add_token(&tokens, buffer, current_type);
+			i++;
+			continue ;
 		}
-		else
+		if (current_type == TOKEN_PIPE)
 		{
-			if (j < sizeof(buffer) - 1)
-				buffer[j++] = input[i];
+			if (j > 0)
+			{
+				buffer[j] = '\0';
+				add_token(&tokens, buffer, TOKEN_WORD);
+				j = 0;
+			}
+			buffer[0] = input[i];
+			buffer[1] = '\0';
+			add_token(&tokens, buffer, TOKEN_PIPE);
+			i++;
+			continue ;
 		}
+		if (current_type == TOKEN_SEMICOLON)
+		{
+			if (j > 0)
+			{
+				buffer[j] = '\0';
+				add_token(&tokens, buffer, TOKEN_WORD);
+				j = 0;
+			}
+			buffer[0] = input[i];
+			buffer[1] = '\0';
+			add_token(&tokens, buffer, TOKEN_SEMICOLON);
+			i++;
+			continue ;
+		}
+		if (j < sizeof(buffer) - 1)
+			buffer[j++] = input[i];
 		i++;
 	}
 	if (j > 0)
