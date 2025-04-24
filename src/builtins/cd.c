cb3221e9f6758922bcff5edf81f6fb428596071c
Hamza <hbelaih@student.42amman.com>
2025-04-24 18:29:01 +0300

    added the cd without no parameters

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,140 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hamzabillah <hamzabillah@student.42.fr>    +#+  +:+       +#+        */
+/*   By: hbelaih <hbelaih@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 23:30:00 by hamzabillah       #+#    #+#             */
-/*   Updated: 2025/04/21 23:23:55 by hamzabillah      ###   ########.fr       */
+/*   Updated: 2025/04/24 18:28:20 by hbelaih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static int	update_env_var(t_env **env, const char *name, const char *value)
+int builtin_cd(t_token *tokens, char **env_array)
 {
-	t_env	*current;
-	t_env	*new;
-	char	*temp;
-	char	*new_var;
+    char *dir_path = NULL;
+    char current_dir[PATH_MAX];
+    char new_dir[PATH_MAX];
+    int ret = 0;
 
-	temp = ft_strjoin(name, "=");
-	if (!temp)
-		return (1);
-	new_var = ft_strjoin(temp, value);
-	free(temp);
-	if (!new_var)
-		return (1);
-	current = *env;
-	while (current)
-	{
-		if (ft_strncmp(current->value, name, ft_strlen(name)) == 0
-			&& current->value[ft_strlen(name)] == '=')
-		{
-			free(current->value);
-			current->value = new_var;
-			return (0);
-		}
-		current = current->next;
-	}
-	new = malloc(sizeof(t_env));
-	if (!new)
-	{
-		free(new_var);
-		return (1);
-	}
-	new->value = new_var;
-	new->next = *env;
-	*env = new;
-	return (0);
-}
+    init_global_env(env_array);
+    
+    if (getcwd(current_dir, PATH_MAX) == NULL)
+    {
+        perror("minishell: cd");
+        return (1);
+    }
 
-static char	*get_env_value_from_list(const char *name, t_env *env)
-{
-	t_env	*current;
+    if (!tokens->next)
+    {
+        dir_path = get_env_value("HOME", env_array);
+        if (!dir_path)
+        {
+            ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
+            return (1);
+        }
+    }
+    else if (ft_strncmp(tokens->next->value, "-", 2) == 0)
+    {
+        dir_path = get_env_value("OLDPWD", env_array);
+        if (!dir_path)
+        {
+            ft_putstr_fd("minishell: cd: OLDPWD not set\n", STDERR_FILENO);
+            return (1);
+        }
+        ft_putendl_fd(dir_path, STDOUT_FILENO); 
+    }
+    else
+    {
+        dir_path = tokens->next->value;
+    }
 
-	current = env;
-	while (current)
-	{
-		if (ft_strncmp(current->value, name, ft_strlen(name)) == 0
-			&& current->value[ft_strlen(name)] == '=')
-			return (current->value + ft_strlen(name) + 1);
-		current = current->next;
-	}
-	return (NULL);
-}
+    if (chdir(dir_path) == -1)
+    {
+        ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+        ft_putstr_fd(dir_path, STDERR_FILENO);
+        ft_putstr_fd(": ", STDERR_FILENO);
+        perror("");
+        return (1);
+    }
 
-static char	*get_target_dir(t_token *tokens, t_env *env, char **old_pwd)
-{
-	char	*home;
+    if (getcwd(new_dir, PATH_MAX) == NULL)
+    {
+        perror("minishell: cd");
+        return (1);
+    }
 
-	if (!tokens->next)
-	{
-		home = get_env_value_from_list("HOME", env);
-		if (!home)
-		{
-			ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
-			return (NULL);
-		}
-		return (ft_strdup(home));
-	}
-	if (ft_strncmp(tokens->next->value, "-", 2) == 0)
-	{
-		*old_pwd = get_env_value_from_list("OLDPWD", env);
-		if (!*old_pwd)
-		{
-			ft_putstr_fd("minishell: cd: OLDPWD not set\n", STDERR_FILENO);
-			return (NULL);
-		}
-		return (ft_strdup(*old_pwd));
-	}
-	return (ft_strdup(tokens->next->value));
-}
-
-int	builtin_cd(t_token *tokens, char **env_array)
-{
-	static t_env *env = NULL;
-	char *dir_path;
-	char *old_pwd;
-	char *new_pwd;
-	char cwd[PATH_MAX];
-
-	if (!env)
-	{
-		env = init_env(env_array);
-		if (!env)
-			return (1);
-	}
-	old_pwd = getcwd(cwd, PATH_MAX);
-	if (!old_pwd)
-	{
-		perror("minishell: cd");
-		return (1);
-	}
-	dir_path = get_target_dir(tokens, env, &old_pwd);
-	if (!dir_path)
-		return (1);
-	if (chdir(dir_path) == -1)
-	{
-		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-		ft_putstr_fd(dir_path, STDERR_FILENO);
-		ft_putstr_fd(": ", STDERR_FILENO);
-		perror("");
-		free(dir_path);
-		return (1);
-	}
-	new_pwd = getcwd(cwd, PATH_MAX);
-	if (!new_pwd)
-	{
-		perror("minishell: cd");
-		free(dir_path);
-		return (1);
-	}
-	if (update_env_var(&env, "OLDPWD", old_pwd) != 0 || update_env_var(&env,
-			"PWD", new_pwd) != 0)
-	{
-		free(dir_path);
-		return (1);
-	}
-	free(dir_path);
-	return (0);
+    t_token export_token;
+    t_token oldpwd_token;
+    char *oldpwd_value = ft_strjoin("OLDPWD=", current_dir);
+    
+    if (!oldpwd_value)
+        return (1);
+        
+    export_token.value = "export";
+    export_token.next = &oldpwd_token;
+    oldpwd_token.value = oldpwd_value;
+    oldpwd_token.next = NULL;
+    
+    ret = builtin_export(&export_token, env_array);
+    free(oldpwd_value);
+    
+    char *pwd_value = ft_strjoin("PWD=", new_dir);
+    if (!pwd_value)
+        return (1);
+        
+    oldpwd_token.value = pwd_value;
+    
+    ret |= builtin_export(&export_token, env_array);
+    free(pwd_value);
+    
+    return (ret);
 }
